62590961050bec13668ea2b809db9dbfd0a32735
cosbi <e4903180@gmail.com>
2023-05-17 17:05:49 +0800

    0517 edit Data

--- Data/LoadData.py
+++ Data/LoadData.py
@@ -6,40 +6,27 @@ import numpy as np
 
 class LoadData:
 
-    def __init__(self) -> None:
-        self.train_data = np.ndarray([])
-        self.test_data = np.ndarray([])
+    def __init__(self, n_slide, data_range) -> None:
+        self.train_data = np.ndarray(shape=(n_slide,data_range), dtype=float)
+        self.test_data = np.ndarray(shape=(n_slide,data_range), dtype=float)
         self.date_data_start_list = []
         self.date_predict_start_list = []
         self.date_predict_end_list = []
         self.test_data_start_list = []
 
-    def load_data(self, stock_name, date_predict_start, data_range, slide_range, n_slide):
+    def load_and_split_data(self, stock_name, date_predict_start, data_range, slide_range, n_slide):
         all_data = yf.Ticker(stock_name).history(period='max')
         all_data.index = all_data.index.date
+        # all_data['Close'].iloc[0:len(all_data['Close'])] = list(range(0, len(all_data['Close'])))
         date_predict_start = self._check_start(date_predict_start, all_data)
         predict_start = all_data.index.get_loc(date_predict_start)
         for i in tqdm(range(n_slide)):
             predict_end = predict_start + data_range
-            date_predict_end = all_data.iloc[predict_end].name
             data_start = predict_start - data_range
-            date_data_start = all_data.iloc[data_start].name
-            # print(type(all_data['Close'].iloc[data_start:predict_start].values))
             self.train_data[i, :] = all_data['Close'].iloc[data_start:predict_start].values
             self.test_data[i, :] = all_data['Close'].iloc[predict_start:predict_end].values
-
-            # date_data_start_list.append(date_data_start)
-            # date_predict_start_list.append(date_predict_start)
-            # date_predict_end_list.append(date_predict_end)
-            # test_data_start_list.append(test_data['data_' + str(i)].index[0])
-            # data_start = data_start + slide_range
-            # predict_start = predict_start + slide_range
-            # train_data['data_' + str(i)] = train_data['data_' +
-            #                                         str(i)].reset_index(drop=False)
-            # test_data['data_' + str(i)] = test_data['data_' +
-            #                                         str(i)].reset_index(drop=False)
-
-        # return self.train_data, self.test_data, all_data, self.test_data_start_list
+            data_start = data_start + slide_range
+            predict_start = predict_start + slide_range
         return self.train_data, self.test_data
 
     def _check_start(self, date_predict_start, all_data):
@@ -51,29 +38,15 @@ class LoadData:
                 relativedelta(days=+1)
             check = date_predict_start in list(all_data.index)
         return date_predict_start
-        
+
+
 if __name__ == '__main__':
     stock_name = "^GSPC"
     date_predict_start = '2020-01-01'
     data_range = 10
-    slide_range = 5
+    slide_range = 10
     n_slide = 5
-    # train_data, test_data, all_data, test_data_start_list=\
-    #     LoadData.load_data(stock_name, date_predict_start, data_range, slide_range, n_slide)
-    # print(train_data)
-    # print(test_data)
-    # print(all_data)
-    # print(test_data_start_list)
-    all_data = yf.Ticker(stock_name).history(period='max')
-    # all_data.index = all_data.index.date
-    train_data = np.ndarray(shape=(n_slide,data_range), dtype=float)
-    start = 0
-    for i in range(n_slide):
-        train_data[i, :] = all_data['Close'].iloc[start:start+data_range].values
-        start += slide_range
-    print(all_data[0:100])
-    # Dataloader = LoadData()
-    # train_data, test_data = Dataloader.load_data(stock_name, date_predict_start, data_range, slide_range, n_slide)
-    # print(train_data)
-    # print(check_start(date_predict_start, all_data))
-    # print(type(all_data.index[0]) == datetime.date)
+    dataloader = LoadData(n_slide, data_range)
+    train_data, test_data =\
+        dataloader.load_and_split_data(stock_name, date_predict_start, data_range, slide_range, n_slide)
+    print(train_data)
